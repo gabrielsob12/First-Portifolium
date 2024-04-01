95bb40e02d32dfbc68b6747aa2ef26e954201fbe
gabrielsob12 <gabrielsob2121@gmail.com>
2024-04-01 19:03:52 -0400

    portifolio
    > webpage design complete
    > frontpage interactions complete
    > links and images complete
    > missing child pages

--- javascript.js
+++ javascript.js
@@ -1,26 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
     var scheduleLink = document.getElementById('schedule-link');
-    var homeSci = document.querySelector('.home-sci-2');
+    var hireMeLink = document.getElementById('hire-link');
+    var moreInfoLink = document.getElementById('moreinfo-link');
 
-    scheduleLink.addEventListener('click', function(event) {
-        event.preventDefault(); // Evita o comportamento padrão do link
-        showIcons();
-        homeSci.classList.add('icons-visible');
+    var homeSci1 = document.querySelector('.home-sci-2');
+    var homeSci2 = document.querySelector('.home-sci-1');
+    var homeSci3 = document.querySelector('.home-sci-3');
+
+    var iconsVisible = false;
+
+    scheduleLink.addEventListener('mouseenter', function() {
+        showIcons(homeSci1);
     });
 
-    document.addEventListener('click', function(event) {
-        // Verifica se o clique ocorreu fora da área do link "Schedule"
-        if (event.target !== scheduleLink && !homeSci.contains(event.target)) {
+    hireMeLink.addEventListener('mouseenter', function() {
+        showIcons(homeSci2);
+    });
+
+    moreInfoLink.addEventListener('mouseenter', function() {
+        showIcons(homeSci3);
+    });
+
+    document.addEventListener('mouseleave', function(event) {
+        if (!isMouseOverElement(scheduleLink) && !isMouseOverElement(hireMeLink) && !isMouseOverElement(moreInfoLink)) {
             hideIcons();
-            homeSci.classList.remove('icons-visible');
         }
     });
+
+    function isMouseOverElement(element) {
+        var rect = element.getBoundingClientRect();
+        return event.clientX >= rect.left && event.clientX <= rect.right && event.clientY >= rect.top && event.clientY <= rect.bottom;
+    }
+
+    function hideIcons() {
+        homeSci1.style.display = 'none';
+        homeSci2.style.display = 'none';
+        homeSci3.style.display = 'none';
+        iconsVisible = false;
+    }
+
+    function showIcons(iconContainer) {
+        hideIcons();
+        iconContainer.style.display = 'flex';
+        iconsVisible = true;
+    }
 });
-
-function hideIcons(){
-    document.querySelector('.home-sci-2').style.display = 'none';
-}
-
-function showIcons() {
-    document.querySelector('.home-sci-2').style.display = 'flex';
-}
